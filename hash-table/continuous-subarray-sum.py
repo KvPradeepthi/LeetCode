e574bb30ab99ea7a21a45bf467a7054d5469dcdb
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-02-24 09:56:30 +0530

    LC-continuous-subarray-sum [Runtime: N/A; Memory: N/A]

--- hash-table/continuous-subarray-sum.py
+++ hash-table/continuous-subarray-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n=len(nums)
-        p=1
+        sum=0
         for i in range(0,n):
-            p*=nums[i]
-            if p%k==0:
+            sum+=nums[i]
+            if sum%k==0:
                 return True
                 break
         return False
