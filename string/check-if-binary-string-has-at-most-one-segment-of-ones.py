8d59347d8703e71ddd3587f8800390df5e052e6d
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-03-06 13:50:00 +0530

    LC-check-if-binary-string-has-at-most-one-segment-of-ones [Runtime: 0 ms; Memory: 19.2 MB]

--- string/check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ string/check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,10 +1,7 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        if len(s)==1 and s=='1':
-            return True
-        for i in range(len(s)-1):
-            if (s[i]=='1'and s[i+1]=='1') or (count(1)==1):
-                return True
-            else:
+        for i in range(1,len(s)):
+            if s[i]=='1'and s[i-1]=='0':
                 return False
+        return True
         
