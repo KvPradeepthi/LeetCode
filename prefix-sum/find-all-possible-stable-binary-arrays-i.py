c3e6e575ccd408fb9266e9a6ba4fd82f1278d17b
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-03-09 11:49:02 +0530

    Synchronize existing submission history (1/4)

---
+++ prefix-sum/find-all-possible-stable-binary-arrays-i.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 10**9 + 7
+        
+        dp = [[[0]*2 for _ in range(one+1)] for _ in range(zero+1)]
+        
+        for i in range(1, min(zero, limit)+1):
+            dp[i][0][0] = 1
+            
+        for j in range(1, min(one, limit)+1):
+            dp[0][j][1] = 1
+        
+        for i in range(zero+1):
+            for j in range(one+1):
+                
+                for k in range(1, limit+1):
+                    if i-k >= 0:
+                        dp[i][j][0] = (dp[i][j][0] + dp[i-k][j][1]) % MOD
+                    else:
+                        break
+                
+                for k in range(1, limit+1):
+                    if j-k >= 0:
+                        dp[i][j][1] = (dp[i][j][1] + dp[i][j-k][0]) % MOD
+                    else:
+                        break
+        
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD
+        
