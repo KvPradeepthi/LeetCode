92c32323dfad48f3b6ae5b4443be87e5c7ec055d
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-05-05 18:28:23 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.py
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,12 +2,13 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         n=len(nums)
         low,high=0,n-1
-        if n==1:
-            return 0,0
         while low<=high:
             mid=(low+high)//2
             if nums[mid]==target:
-                return mid,mid+1
+                if n>1:
+                    return mid,mid+1
+                if n==1:
+                    return mid,mid
             elif nums[mid]<target:
                 low=low+1
             elif nums[mid]>target:
