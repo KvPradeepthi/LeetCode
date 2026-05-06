5806605577f0918e33a2cdcd8d5e388eb3f7b0b7
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-05-06 10:22:12 +0530

    LC-binary-search [Runtime: 12 ms; Memory: 20.4 MB]

---
+++ binary-search/binary-search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low,high=0,n-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                low=low+1
+            elif nums[mid]>target:
+                high=high-1
+        return -1
+            
