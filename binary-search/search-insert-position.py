c47d76de6d7e0d9eb2b8fb215cdfd43c99f6ea5c
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-05-06 11:03:52 +0530

    LC-search-insert-position [Runtime: 0 ms; Memory: 19.9 MB]

--- binary-search/search-insert-position.py
+++ binary-search/search-insert-position.py
@@ -1,17 +1,15 @@
-from typing import List
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
+        n=len(nums)
+        low,high=0,n-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+            elif nums[mid]<target:
+                low=low+1
+            elif nums[mid]>target:
+                high=high-1
+        return low
         
-        # If target not found, left will be the insertion position
-        return left
+       
