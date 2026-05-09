cbcf69c9d2343640bb52f5af94f997627dc92d50
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-05-09 23:15:39 +0530

    LC-min-stack [Runtime: N/A; Memory: N/A]

--- design/min-stack.py
+++ design/min-stack.py
@@ -1,32 +1,25 @@
 class MinStack:
+
     def __init__(self):
         self.stack = []
-        self.min_stack = []
+        self.minStack = []
 
     def push(self, val: int) -> None:
+
         self.stack.append(val)
-        if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)
+
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
 
     def pop(self) -> None:
-        if self.stack:
-            val = self.stack.pop()
-            if val == self.min_stack[-1]:
-                self.min_stack.pop()
+
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+
+        self.stack.pop()
 
     def top(self) -> int:
-        if self.stack:
-            return self.stack[-1]
-        return None
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        if self.min_stack:
-            return self.min_stack[-1]
-        return None
-
-# Your MinStack obect will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return self.minStack[-1]
