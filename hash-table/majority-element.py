113af059179aa51574a0c7bd86e508dd76a43695
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-07-22 15:40:10 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- hash-table/majority-element.py
+++ hash-table/majority-element.py
@@ -1,8 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         sums=0
+        s=0
         for i in nums:
             if nums.count(i)>sums:
+                s=count(i)
                 sums=i
         return sums
             
