10b7e9383433121de64aed248f1a03097b308906
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-07-26 18:43:37 +0530

    Time: 9 ms (83.57%), Space: 19.3 MB (68.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,17 +6,10 @@ class Solution:
             if ch in seen and seen[ch]>=left:
                 left=seen[ch]+1
             seen[ch]=right
-            window_len=right-left+1
-            if window_len>res:
-                res=window_len
+            window=right-left+1
+            if window>res:
+                res=window
         return res
 
 
-
-
-
-
-
-        
-        
         
