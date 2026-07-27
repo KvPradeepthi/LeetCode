8b0ef51b31b27b3cd9b3ecd4f8abe149acbd43a5
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-07-27 06:40:39 +0530

    LC-koko-eating-bananas [Runtime: N/A; Memory: N/A]

--- array/koko-eating-bananas.py
+++ array/koko-eating-bananas.py
@@ -1,22 +1,18 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low, high = 1, max(piles)
-        res = high
-
-        while low <= high:
-
-            k = (low + high) // 2
-
-            hours = 0
-
+        low,high=1,max(piles)
+        res=high
+        while low<=high:
+            k=(low+high)//2
+            hours=0
             for p in piles:
-                hours += math.ceil(p / k)
-
-            if hours <= h:
-                res = min(res, k)
-                high = k - 1
-
+                hours+=math.ceil(p/k)
+            if hours<=h:
+                res=min(k,res)
+                high=k-1
             else:
-                low = k + 1
-
+                low=k+1
         return res
+
+
+        
