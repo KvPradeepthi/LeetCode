5915c32dcde0ea5b7f8a04331917df129d7aea59
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-08-17 09:43:59 +0530

    LC-squares-of-a-sorted-array [Runtime: N/A; Memory: N/A]

--- array/squares-of-a-sorted-array.py
+++ array/squares-of-a-sorted-array.py
@@ -1,8 +1,21 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        num=[]
-        for i in nums:
-            i=i*i
-            num.append(i)
-        num.sort()
-        return num
+        n=len(nums)
+        res=[0]*n
+        left=0
+        right=n-1
+        pos=n-1
+        while left<=right:
+            left_square=nums[left]**2 
+            right_square=nums[right]**2
+            if left_square>right_square:
+                res[pos]=left_square
+                left+=1
+            else:
+                res[pos]=right_square 
+                right-=1
+            pos-=1
+        return res
+
+
+        
