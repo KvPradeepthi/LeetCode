77908475a1958aca314881579dd8ad1f865bdb4c
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-08-20 10:13:59 +0530

    LC-intersection-of-two-arrays-ii [Runtime: N/A; Memory: N/A]

--- sorting/intersection-of-two-arrays-ii.py
+++ sorting/intersection-of-two-arrays-ii.py
@@ -1,9 +1,13 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums3=[]
+        freq={}
         for i in nums1:
-            if i in nums2:
+            freq[i]=freq.get(i,0)+1
+        for i in nums2:
+            if freq.get(i,0)>0:
                 nums3.append(i)
+                freq[i]-=1
         return nums3
 
         
