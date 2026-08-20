e3ecd3678d9b4c7e3515a1aa82f3d3832850385e
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-08-20 10:05:40 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- sorting/majority-element.py
+++ sorting/majority-element.py
@@ -1,16 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mp={}
-        s=0
-        for i in nums:
-            if i in mp:
-                mp[i]+=1
-            else:
-                mp[i]=1
-        max_freq=0
-        ans=0
-        for key in mp:
-            if mp[key]>max_freq:
-                max_freq=mp[key]
-                ans=key
-        return ans  
+        freq={}
+        n=len(nums)
+        for ch in nums:
+            freq[ch]=freq.get(ch,0)+1
+        for ch in nums:
+            if freq[ch]>(n//2):
+                return ch 
+
+        
