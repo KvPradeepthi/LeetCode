9b25528d4206a1262af313cc3ca1d2d1104fb450
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2026-08-26 08:46:17 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

---
+++ hash-table/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x:nums)
+        {
+            mp[x]++;
+        }
+        for (int i=0;i<nums.size();i++)
+        {
+            if (mp[nums[i]]>=2)
+            {
+            return true;
+            }
+        }
+        return false;
+    }
+};
